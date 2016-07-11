8885a4f0f10961a0f4d06ea499b86cc8ee0ec272
龚爱荣 <gongairong@gongairongdeimac.local>
2016-07-11 13:40:33 +0800

    初始化

---
+++ test3/test3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test3
+//
+//  Created by 龚爱荣 on 16/7/11.
+//  Copyright © 2016年 JZM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test3/test3/TwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwoViewController.h
+//  test3
+//
+//  Created by 龚爱荣 on 16/7/11.
+//  Copyright © 2016年 JZM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwoViewController : UIViewController
+
+@end
